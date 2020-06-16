6451a63fa41e15ee2681e2c8e7f1ee0d68d3a026
DivyaPathrabe <66524875+DivyaPathrabe@users.noreply.github.com>
2020-06-16 11:39:33 +0530

    created stairs.c
    This file prints the output like 6 stairs in sequence on the screen.

---
+++ stairs.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+main()
+{
+int i,j;
+  for(i=01;i<=6;i++)
+  {
+    for(j=01;j<=i;j++)
+    {
+      printf("%d",(j&01));
+      }
+      printf("\n");
+      }
+      return 0;
+      }
